8c66b7bca3d7a6e891cace49c91408f39fcc4980
Deekshit10 <deekshitg101@gmail.com>
2025-01-15 12:40:28 +0530

    exploring different http methods

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send("{fullName: Deekshit}");
+});
+
+app.post("/user", (req, res) => {
+  res.send("User data stored in database");
+});
+
+app.put("/user", (req, res) => {
+  res.send("User Updated succesfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully");
+});
+
 app.listen(3000, () => {
   console.log("app listening on port 3000");
 });

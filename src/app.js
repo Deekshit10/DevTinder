4fdd800fb330480eda5899972945e88f373ed68c
Deekshit10 <deekshitg101@gmail.com>
2025-01-16 22:59:17 +0530

    Update and delete api

--- src/app.js
+++ src/app.js
@@ -42,6 +42,29 @@ app.get("/feed", async (req, res) => {
     res.status(400).send("something went wrong");
   }
 });
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+
+    res.send("user deleted succesfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
 connectdb()
   .then(() => {
     console.log("Database connected succesfully");
